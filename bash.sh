16e989d1bea09bacbd065ff9795759f2ab6abed6
xkm <xkm@xkm.be>
2026-05-13 21:51:36 +0800

    update template and result of issue#1 on test.md

--- bash.sh
+++ bash.sh
@@ -2,48 +2,38 @@
 gh issue view 1 --json \
 number,title,body,author,assignees,labels,milestone,state,stateReason,createdAt,updatedAt,closed,closedAt,url,comments \
 --jq '
-  def bump_headings:
-    if . == null or . == "" then .
-    else
-      split("\n")
-      | map(if test("^#+[[:space:]]") then "#" + . else . end)
-      | join("\n")
-    end;
-
   def list_or_none(stream):
     if (stream | length) > 0 then stream else ["- None"] end;
 
-  .body |= bump_headings
-  | .comments |= map(.body |= bump_headings)
-  | [
-      ("# Issue #" + (.number|tostring) + ": " + .title),
+  [
+    ("# Issue #" + (.number|tostring) + ": " + .title),
+    "",
+    ("- URL: " + .url),
+    ("- State: " + .state + (if .stateReason then " (" + .stateReason + ")" else "" end)),
+    ("- Author: " + .author.login),
+    ("- Created: " + .createdAt),
+    ("- Updated: " + .updatedAt)
+  ]
+  + (if .closed then [("- Closed: " + .closedAt)] else [] end)
+  + list_or_none(.assignees | map("- Assignee: " + .login + (if .name then " (" + .name + ")" else "" end)))
+  + list_or_none(.labels | map("- Label: " + .name))
+  + [("- Milestone: " + (if .milestone then .milestone.title else "None" end))]
+  + [
       "",
-      ("- URL: " + .url),
-      ("- State: " + .state + (if .stateReason then " (" + .stateReason + ")" else "" end)),
-      ("- Author: " + .author.login),
-      ("- Created: " + .createdAt),
-      ("- Updated: " + .updatedAt)
+      (if .body then .body else "_No description_" end)
     ]
-    + (if .closed then [("- Closed: " + .closedAt)] else [] end)
-    + list_or_none(.assignees | map("- Assignee: " + .login + (if .name then " (" + .name + ")" else "" end)))
-    + list_or_none(.labels | map("- Label: " + .name))
-    + [("- Milestone: " + (if .milestone then .milestone.title else "None" end))]
-    + [
+  + (if (.comments | length) > 0 then
+      (.comments | map([
         "",
-        "# Description",
-        (if .body then .body else "_No description_" end)
-      ]
-    + (if (.comments | length) > 0 then
-        .comments
-        | map([
-            "",
-            ("# " + .author.login + " commented at " + .createdAt),
-            "",
-            (if .body then .body else "_No comment body_" end)
-          ] | join("\n"))
-      else
-        ["", "# Comments", "_No comments_"]
-      end)
-    | join("\n")
+        ("# Author: " + .author.login),
+        "",
+        ("Created: " + .createdAt),
+        "",
+        (if .body then .body else "_No comment body_" end)
+      ] | join("\n")))
+    else
+      []
+    end)
+  | join("\n")
 '
 
---
+++ test.md
@@ -0,0 +1,85 @@
+# Issue #1: 测试ISSUE
+
+- URL: https://github.com/xkmtestorg/issue2md/issues/1
+- State: OPEN ()
+- Author: dreamstarsky
+- Created: 2026-05-13T13:37:05Z
+- Updated: 2026-05-13T13:43:52Z
+- None
+- None
+- Milestone: None
+
+# 标题1
+
+## 2
+
+### 3
+
+#### 4
+ 
+##### 5
+
+###### 6
+
+####### 7？
+
+# 标题2
+
+# Author: dreamstarsky
+
+Created: 2026-05-13T13:37:24Z
+
+# 测试comment
+
+## 2
+
+### 3
+
+#### 4
+ 
+##### 5
+
+###### 6
+
+####### 7？
+
+# 标题2
+
+# Author: dreamstarsky
+
+Created: 2026-05-13T13:37:33Z
+
+# 测试连续comment
+
+## 2
+
+### 3
+
+#### 4
+ 
+##### 5
+
+###### 6
+
+####### 7？
+
+# 标题2
+
+# Author: dreamstarsky
+
+Created: 2026-05-13T13:42:36Z
+
+# 测试```下的#
+
+```text
+# 特殊情况
+```
+
+# Author: dreamstarsky
+
+Created: 2026-05-13T13:43:52Z
+
+# 测试非行首# 
+
+
+   # 2123
