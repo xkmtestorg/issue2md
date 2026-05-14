f02b347ed626f4227b6a3f7fa4b8736deaffd95a
xkm <xkm@xkm.be>
2026-05-14 18:13:41 +0800

    simple the template

--- bash.sh
+++ bash.sh
@@ -1,39 +1,16 @@
 #!/usr/bin/env bash
-gh issue view 1 --json \
-number,title,body,author,assignees,labels,milestone,state,stateReason,createdAt,updatedAt,closed,closedAt,url,comments \
---jq '
-  def list_or_none(stream):
-    if (stream | length) > 0 then stream else ["- None"] end;
 
-  [
-    ("# Issue #" + (.number|tostring) + ": " + .title),
-    "",
-    ("- URL: " + .url),
-    ("- State: " + .state + (if .stateReason then " (" + .stateReason + ")" else "" end)),
-    ("- Author: " + .author.login),
-    ("- Created: " + .createdAt),
-    ("- Updated: " + .updatedAt)
-  ]
-  + (if .closed then [("- Closed: " + .closedAt)] else [] end)
-  + list_or_none(.assignees | map("- Assignee: " + .login + (if .name then " (" + .name + ")" else "" end)))
-  + list_or_none(.labels | map("- Label: " + .name))
-  + [("- Milestone: " + (if .milestone then .milestone.title else "None" end))]
-  + [
-      "",
-      (if .body then .body else "_No description_" end)
-    ]
-  + (if (.comments | length) > 0 then
-      (.comments | map([
-        "",
-        ("# Author: " + .author.login),
-        "",
-        ("Created: " + .createdAt),
-        "",
-        (if .body then .body else "_No comment body_" end)
-      ] | join("\n")))
-    else
-      []
-    end)
-  | join("\n")
+gh issue view https://github.com/xkmtestorg/issue2md/issues/1 \
+  --json 'assignees,author,body,closed,closedAt,closedByPullRequestsReferences,comments,createdAt,id,isPinned,labels,milestone,number,reactionGroups,state,stateReason,title,updatedAt,url' \
+  --template \
+'# {{ .title }}
+> {{ .author.login }} create at {{ .createdAt }}
+
+{{.body}}
+
+{{ range .comments }}---
+> {{ .author.login }} comment at {{ .createdAt }}
+
+{{ .body }}
+{{ end }}
 '
-
